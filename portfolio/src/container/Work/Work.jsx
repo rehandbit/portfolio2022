cd5eb2da081469d11931b63f66d997e27722c325
rehandbit <rehan.farazmdp@gmail.com>
2022-04-14 00:46:28 +0530

    styling fixed

--- portfolio/src/container/Work/Work.jsx
+++ portfolio/src/container/Work/Work.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect} from 'react'
-import { AiFilleye, AiFillGithub } from 'react-icons/ai'
+import { AiFillEye, AiFillGithub } from 'react-icons/ai'
 import { motion } from 'framer-motion'
 import { AppWrapper } from '../../wrapper'
 import { urlFor, client } from '../../client'
@@ -27,15 +27,15 @@ const Work = () => {
 	<>
 		<h2 className='head-text'>
 			My Creative 
-			<span> Portfolio</span>
-			<br/>
+			<span> Portfolio </span>
 			Section
 		</h2>
 		<div className='app__work-filter'>
-			{['React JS','Web development','UI/UX','Mobile App'].map((item, index) => (
+			{['UI/UX','Web App','Mobile App','React JS','All'].map((item, index) => (
 				<div 
+					key={index} 
+					onClick={() => handlework(item)} 
 					className={`app__work-filter-item app__flex p-text ${activeFilter === item ? 'item-active' : ''}`} 
-					key={index} onclick={() => handlework(item)} 
 				>{item}</div>
 			))}
 		</div>
@@ -48,11 +48,46 @@ const Work = () => {
 			<div className='app__work-item app__flex' key={index}>
 				<div className='app__work-img app__flex'>
 					<img src={urlFor(work.imgUrl)} alt={work.name} />
+					
+					<motion.div
+						className='app__work-hover app__flex'
+						whileHover={{opacity:[0, 1]}}
+						transition={{ duration: 0.25, ease: 'easeInOut', staggerChildren: 0.5}}
+					>
+						<a href={work.projectLink} target="_blank" rel="noreferrer">
+							<motion.div
+								whielInView={{scale:[0, 1]}}
+								whileHover={{scale:[1, 0.9]}}
+								transition={{duration: 0.25}}
+								className='app__flex'
+							>
+								<AiFillEye />
+							</motion.div>
+						</a>
+						<a href={work.codeLink} target="_blank" rel="noreferrer">
+							<motion.div
+								whielInView={{scale:[0,1]}}
+								whileHover={{scale:[1, 0.9]}}
+								transition={{duration: 0.25}}
+								className='app__flex'
+							>
+								<AiFillGithub />
+							</motion.div>
+						</a>
+					</motion.div>
 				</div>
+				
+				<div className='app__work-content app__flex'>
+						<h4 className='bold-text'>{work.title}</h4>
+						<p className='p-text' style={{marginTop: 10}}>{work.description}</p>
+						<div className='app__work-tag app__flex'>  
+							<p className='p-text'>{work.tags[0]}</p>
+						</div>
+					</div>
 			</div>
 		))}
 		</motion.div>
 	</>
 	)
 }
-export default Work
+export default AppWrapper(Work,'work');
